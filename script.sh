867103d1f5d267df8eedc0538f280dd90c109327
Owen Collier-Ridge <owen.collier.ridge@gmail.com>
2020-08-03 19:51:22 -0400

    Init repo

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir dist
+cp src/* dist/
+pip install -r requirements.txt -t dist/
+
+aws cloudformation package --template-file template.yaml --output-template processed.template.yaml --s3-bucket "owen-lambda-bucket" --s3-prefix list-instances
+aws cloudformation deploy --template-file processed.template.yaml --stack-name header-api --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region us-east-1
+
+rm processed.template.yaml
+rm -rf dist/
