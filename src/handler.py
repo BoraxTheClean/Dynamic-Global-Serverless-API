1c1026b2abda48d2c3c18be9e2b0a2a6597c41e9
Owen Collier-Ridge <owen.collier.ridge@gmail.com>
2020-08-08 19:36:39 +0000

    Add references, bug fix

--- src/handler.py
+++ src/handler.py
@@ -2,5 +2,9 @@ import json
 
 def handler(event,context):
     print(event)
-    headers = [i+':'+j for i,j in event['headers'].items()]
-    return {'statusCode':200,'body':'\n'.join(headers)}
+    headers = ['<html>']
+    headers.extend([i+':'+j+'<br/>' for i,j in event['headers'].items() if i.startswith('CloudFront-Viewer')])
+    headers.append("<a href=\"blog.owen.dev\">Blog</a>")
+    headers.append("<a href=\"owen.dev\">Site</a>")
+    headers.append('</html>')
+    return {'statusCode':200,'body':'\n'.join(headers),'headers':{'Content-Type':'text/html'}}
