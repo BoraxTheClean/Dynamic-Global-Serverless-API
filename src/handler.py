275a7e308aa6fe12268e1d4477693e4ce70e155e
Owen Collier-Ridge <owen.collier.ridge@gmail.com>
2020-08-09 18:43:18 +0000

    Use legacy cache settings

--- src/handler.py
+++ src/handler.py
@@ -1,10 +1,9 @@
 import json
 
 def handler(event,context):
-    print(event)
     headers = ['<html>']
-    headers.extend([i+':'+j+'<br/>' for i,j in event['headers'].items() if i.startswith('CloudFront-Viewer')])
-    headers.append("<a href=\"blog.owen.dev\">Blog</a>")
-    headers.append("<a href=\"owen.dev\">Site</a>")
+    headers.extend([i+':'+j+'<br/>' for i,j in event['headers'].items() if i.startswith('CloudFront')])
+    headers.append("<a href=\"https://blog.owen.dev\">Blog</a>")
+    headers.append("<a href=\"https://owen.dev\">Site</a>")
     headers.append('</html>')
     return {'statusCode':200,'body':'\n'.join(headers),'headers':{'Content-Type':'text/html'}}
